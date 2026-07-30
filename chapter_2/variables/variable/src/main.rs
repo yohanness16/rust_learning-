1a3f19b43ae21265e095fa81a1fba04219934a3b
yohanness16 <yohanness1621@gmail.com>
2026-07-30 15:37:57 +0300

    day 2 : variables

---
+++ chapter_2/variables/variable/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut s:i32 = 25;
+    println!(" the length  of the tabel is {s} centimeters ");
+
+    s = 10;
+
+    println!(" the length  of the tabel is {s} centimeters ");
+
+}
