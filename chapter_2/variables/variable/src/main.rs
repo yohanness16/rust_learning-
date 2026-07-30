4542413471f4a3e90dc2137712dc43bc89a6d6b2
yohanness16 <yohanness1621@gmail.com>
2026-07-30 18:25:38 +0300

    variables : end

--- chapter_2/variables/variable/src/main.rs
+++ chapter_2/variables/variable/src/main.rs
@@ -1,9 +1,58 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+//const MIN_AGE: i32 = 18; 
+
+// #[allow(dead_code)]
+type Centimeters = f64;
+
+
+// #[allow(unused_variables)]
 fn main() {
-    let mut s:i32 = 25;
+   /*  let mut s:i32 = 25;
     println!(" the length  of the tabel is {s} centimeters ");
 
     s = 10;
 
     println!(" the length  of the tabel is {s} centimeters ");
+
+// overshadowing 
+
+let size = "10.05";
+println!("The size of the table is {size} centimeters.");
+
+let size = 10.05;
+println!("The size of the table is {size} centimeters.");
+
+let size = 10;
+println!("The size of the table is {size} centimeters.");
+
+
+
+let price = 100;
+
+println!("The price of the table is {price} dollars.");
+
+{
+    println!("The price of the table is {price} dollars.");
+    let price = 200;
+    println!("The price of the table is {price} dollars.");
+}
+
+    
+
+   let MIN_AGE: i32 = 18;
+   println!("The minimum age is {MIN_AGE} years.");
+
+  
+
+    let length : Centimeters = 15.2;
+    let width : Centimeters = 10.5;
+
+    let area : Centimeters = length * width;
+
+    println!("The area of the table is {area} square centimeters.");
+     */
+   //  #[allow(unused_variables)]
+    let x: i32 = 5;
 
 }
